1d1b3f067092f65a02e1a9ef831771272b71525f
fsRoman <51918514+fsRoman@users.noreply.github.com>
2019-06-17 17:42:47 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+$(function() {
+	$('.toggles button').click(function(){
+		var get_id = this.id;
+		var get_current = $('.posts .' + get_id);
+
+		$('.post').hide(0);
+		get_current.show(500);
+	});
+
+	$('#showall').click(function() {
+		$('.post').show(500);
+	});
+});
+
+//.not(get_current)    <-------- ???
+
+
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	center:true,
+  	//items:7,
+  	loop:true,/*
+  	nav:true,/*
+  	dots:true,
+  	/*autoplay:true,
+  	autoplayTimeout:2500,
+  	autoplayHoverPause:true*/
+  	responsive:{
+  		0:{
+  			items:3,
+  			dots:true,
+  			dotsData:true
+  		},
+  		420:{
+  			items:5,
+  			dots:true,
+  			autoplay:true,
+  			autoplayTimeout:2500
+  		},
+  		860:{
+  			items:7,
+  			dots:true,
+  			autoplay:true,
+  			autoplayTimeout:2500,
+  			autoplayHoverPause:true
+  		}
+  	}
+  });
+});
+
+/* Разобраться с адаптивностью карусели!!! Обучение: https://owlcarousel2.github.io/OwlCarousel2/demos/responsive.html*/
