1c35fbfcecd62cb03f4cd7e44814755adef9cdd8
fsRoman <51918514+fsRoman@users.noreply.github.com>
2019-11-18 17:52:54 +0300

    Update script.js

--- color/script.js
+++ color/script.js
@@ -8,18 +8,3 @@ document.getElementById("btnSendTxt").addEventListener('click', function (){
 
 //var x = document.getElementById("fldInpTxt");
 //alert(x);
-
-function lookUpProfile(name, prop){
-    // Only change code below this line
-    for (var x = 0; x < contacts.length; x++) {
-        if (contacts[x].firstName === name) {
-            if (contacts[x].hasOwnProperty(prop)) {
-                return contacts[x][prop];
-            } else {
-                return "No such property";
-            }
-        }
-    }
-    return "No such contact";
-    // Only change code above this line
-    }
